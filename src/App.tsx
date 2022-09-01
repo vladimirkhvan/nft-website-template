da800a69d7136114e77fd7be501dcffad11304d8
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-09-01 10:56:32 +0500

    added display flex to main frame

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import style from './App.module.scss';
 
 import { Navbar } from './components/Navbar';
+import { Header } from './components/Header';
 
 function App() {
-    return <div className={style.main}>
-        <Navbar/>
-    </div>;
+    return (
+        <div className={style.main}>
+            <Navbar />
+            <Header />
+        </div>
+    );
 }
 
 export default App;
